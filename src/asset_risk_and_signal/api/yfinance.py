4b5a0288f536b3d1f9c30f93af220ae19059c297
Brice Nelson <bnelsonemail@icloud.com>
2026-04-19 20:13:49 -0400

    feat: added validation error handling input validation: - is tickers a list - are dates a string - is the list empty - are all elements in the list a string

--- src/asset_risk_and_signal/api/yfinance.py
+++ src/asset_risk_and_signal/api/yfinance.py
@@ -1,7 +1,10 @@
 """A module for interacting with Yahoo Finance API for asset risk and signal analysis."""
+from jedi.inference.helpers import is_string
+from lib2to3.fixer_util import is_list
 from typing import List
 
 import yfinance as yf
+from IPython.utils.wildcard import is_type
 from pandas import DataFrame
 
 
@@ -32,6 +35,16 @@ def multi_yfinance(tickers:list[str], start_date:str, end_date:str) -> DataFrame
     #       - API outages
     #       - empty dataframe (API worked but returned nothing
     #       - unexpected library behavior
-    
+    if not isinstance(tickers,list):
+        raise TypeError("tickers must be passed as a list")
+    if not isinstance(start_date, str) or not isinstance(end_date, str):
+        raise TypeError("Start and end dates must be a string")
+    if len(tickers) == 0:
+        raise ValueError("List of tickers must not be empty")
+    for item in tickers:
+        if not isinstance(item, str):
+            raise TypeError("All tickers must be a string")
+
+
     data: DataFrame = yf.download(tickers, start=start_date, end=end_date)
     return data
